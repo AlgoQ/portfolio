169604df5091860730d7f6b96eb93f3bf9d69eb6
AlgoQ <kobejanssens@outlook.com>
2026-02-04 17:39:08 -0300

    add allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -48,11 +48,10 @@ if not IS_HEROKU_APP:
 # validation of the Host header in the incoming HTTP request. On other platforms you may need
 # to list the expected hostnames explicitly to prevent HTTP Host header attacks. See:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-ALLOWED_HOSTS
-if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["*"]
-else:
-    ALLOWED_HOSTS = []
-
+# if IS_HEROKU_APP:
+    # ALLOWED_HOSTS = ["*"]
+# else:
+ALLOWED_HOSTS = ['algoq.pythonanywhere.com']
 
 # Application definition
 
